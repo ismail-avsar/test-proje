85ee7d9f70726f93c6068113c73107967ee0a464
ismail <i.avsar@outlook.com>
2025-10-18 12:02:11 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, } from 'react-router-dom';
 import Login from './components/Login';
 import Success from './components/Success';
 
 function App() {
   return (
     <Router>
-      <Routes>
+      <Route>
         <Route exact path="/" element={Login} />
         <Route path="/success" element={Success} />
-      </Routes>
+      </Route>
     </Router>
   );
 }
